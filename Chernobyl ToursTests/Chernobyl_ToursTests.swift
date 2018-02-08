df33c800a2c001aa76c20e8afdd38f5aed787035
Maruska <soloeast@imac-2.local>
2018-02-08 23:12:47 +0200

    Initial Commit

---
+++ Chernobyl ToursTests/Chernobyl_ToursTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chernobyl_ToursTests.swift
+//  Chernobyl ToursTests
+//
+//  Created by Maruska on 0802..2018.
+//  Copyright © 2018 Soloeast. All rights reserved.
+//
+
+import XCTest
+@testable import Chernobyl_Tours
+
+class Chernobyl_ToursTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
